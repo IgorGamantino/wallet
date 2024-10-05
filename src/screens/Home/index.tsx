f985e10489ec23c2627f21baca1d75c11437da9f
Igor Amantino <igor.amantino-terceiro@cea.com.br>
2024-10-05 07:00:40 -0300

    feat: add paths map for files

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
-import { Button } from "../../components/Button";
-import { Background } from "../../components/Background";
+import { Button } from "@components/Button";
+import { Background } from "@components/Background";
 import { Container } from "./styles";
-import { Typography } from "../../components/Typography";
+import { Typography } from "@components/Typography";
 
 export function Home() {
   return (
